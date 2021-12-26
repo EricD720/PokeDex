622d245471ea544b7058991508028d953a84e708
EricD720 <68793522+EricD720@users.noreply.github.com>
2021-12-26 18:37:16 -0500

    Updates

---
+++ precache-manifest.8a71a201b3d3fc65425aac6863e7bab6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "17d3a52b8aa12334be04",
+    "url": "/PokeDex/static/css/main.b23d0343.chunk.css"
+  },
+  {
+    "revision": "17d3a52b8aa12334be04",
+    "url": "/PokeDex/static/js/main.17d3a52b.chunk.js"
+  },
+  {
+    "revision": "8ee15efa1a2054ecf258",
+    "url": "/PokeDex/static/js/1.8ee15efa.chunk.js"
+  },
+  {
+    "revision": "cda1be9e1dfcccc18ea0",
+    "url": "/PokeDex/static/js/2.cda1be9e.chunk.js"
+  },
+  {
+    "revision": "7fb2bcccf07346282698",
+    "url": "/PokeDex/static/js/runtime~main.7fb2bccc.js"
+  },
+  {
+    "revision": "5459f9b650067ee4c6b4db9170f0d3cf",
+    "url": "/PokeDex/index.html"
+  }
+];
